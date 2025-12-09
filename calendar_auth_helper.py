7d3993c82847c04686acbdddd67d2bd75811a2a5
brianabarajas <bribarajas24@gmail.com>
2025-12-08 22:10:26 -0800

    Add one-time Google OAuth helper to create token.pickle on the Pi for Calendar access.

---
+++ calendar_auth_helper.py
@@ -0,0 +1,31 @@
+import os
+import pickle
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
+def main():
+    creds = None
+    token_path = os.path.join("PIapp", "token.pickle")
+    cred_path = os.path.join("PIapp", "credentials.json")
+
+    if os.path.exists(token_path):
+        with open(token_path, "rb") as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(cred_path, SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open(token_path, "wb") as token:
+            pickle.dump(creds, token)
+
+    print("token.pickle created at", token_path)
+
+if __name__ == "__main__":
+    main()
